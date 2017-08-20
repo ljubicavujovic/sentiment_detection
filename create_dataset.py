812f54faa8efe55fd8e42597937fcc2e3df358b0
Ljubica Vujovic <ljubica.vujovic@gmail.com>
2017-08-20 21:02:34 +0100

    Added parser for labelled sentences

--- create_dataset.py
+++ create_dataset.py
@@ -11,7 +11,6 @@ class Record:
 
         sd.default.channels = channels
         sd.default.samplerate = self.samplerate
-        print(sd.query_devices())
 
     def record_audio(self, filename):
         record = sd.rec(int(self.duration*self.samplerate), device=1)
@@ -22,25 +21,42 @@ class Record:
         record = np.load(filename + '.npy')
         sd.play(record, blocking=True, device=3)
 
-    def record_positive_audio(self, sample_number):
-        for i in range(0, sample_number):
+    def record_positive_audio(self, begin, sample_number):
+        for i in range(begin, begin + sample_number):
             filename = 'dataset/positive/audio_' + str(i)
             print('Say something positive:')
             self.record_audio(filename)
             print('Stop.')
             self.play_audio(filename)
 
-    def record_negative_audio(self, sample_number):
-        for i in range(0, sample_number):
+    def record_negative_audio(self, begin, sample_number):
+        for i in range(begin, begin + sample_number):
             filename = 'dataset/negative/audio_' + str(i)
-            print('Say something negative:')
+            print('Say : ')
             self.record_audio(filename)
             print('Stop.')
             self.play_audio(filename)
 
+    def list_sentences(self, filenames):
+        open('dataset/positive/positive_sentences.txt', 'w').close()
+        open('dataset/negative/negative_sentences.txt', 'w').close()
+
+        for filename in filenames:
+            with open('dataset/' + filename, 'r') as file:
+                for line in file:
+                    if len(line) < 70:
+                        if int(line[-2]) == 1:
+                            with open('dataset/positive/positive_sentences.txt', 'a') as out:
+                                out.write(line[:-2] + '\n')
+                        else:
+                            with open('dataset/negative/negative_sentences.txt', 'a') as out:
+                                out.write(line[:-2] + '\n')
+
 
 if __name__ == "__main__":
-    record = Record(5, 40000, 1)
-    #record.record_positive_audio(2)
-    # record.record_negative_audio(2)
+    record = Record(5, 40000, 2)
+    #record.list_sentences(['imdb_labelled.txt', 'yelp_labelled.txt', 'amazon_cells_labelled.txt'])
+    #record.record_negative_audio(59, 50)
+    record.play_audio('dataset/negative/audio_98')
+
 
